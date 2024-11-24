dd5dc91578b8ea66a24d25cdc53fdfa3cdfb17b6
vrichie <dev.ritchiek@gmail.com>
2024-11-24 22:31:03 +0300

    Update app title to 'Weather Forecast'

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Weather Forecast',
       theme: ThemeData.dark(useMaterial3: true),
       debugShowCheckedModeBanner: false,
       home: const WeatherScreen(),
